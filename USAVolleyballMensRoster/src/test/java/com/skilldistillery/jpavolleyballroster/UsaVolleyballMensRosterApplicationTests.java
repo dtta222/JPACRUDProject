e243392ff4662885235b79428b8e9ff8bcd41a93
dtta222 <david.t.taylor222@gmail.com>
2023-12-04 10:28:29 -0500

    update project

--- USAVolleyballMensRoster/src/test/java/com/skilldistillery/jpavolleyballroster/UsaVolleyballMensRosterApplicationTests.java
+++ USAVolleyballMensRoster/src/test/java/com/skilldistillery/jpavolleyballroster/UsaVolleyballMensRosterApplicationTests.java
@@ -1,13 +1,58 @@
 package com.skilldistillery.jpavolleyballroster;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class UsaVolleyballMensRosterApplicationTests {
+import com.skilldistillery.jpavolleyballroster.entities.Player;
 
-	@Test
-	void contextLoads() {
-	}
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
+class PlayerTest {
+    private static EntityManagerFactory emf;
+    private EntityManager em;
+    private Player player;
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        emf = Persistence.createEntityManagerFactory("JPAVolleyball");
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        emf.close();
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+        em = emf.createEntityManager();
+        player = em.find(Player.class, 1);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        em.close();
+        player = null;
+    }
+
+    @Test
+    void test_Player_entity_mapping() {
+        assertNotNull(player);
+
+        assertEquals(1, player.getId());
+        assertEquals("Matt", player.getFirstName());
+        assertEquals("Anderson", player.getLastName());
+        assertEquals("1", player.getJerseyNumber());
+        assertEquals("Opposite", player.getPosition());
+        assertEquals("6-10", player.getHeight());
+        assertEquals("08-18-1987", player.getBirthDate());
+    }
 }
+
